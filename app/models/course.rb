d6a68085250764ac114554dd7a31ae0f3bbc6ead
Luiz <silva.luiz@smartfit.com>
2022-01-16 00:38:29 -0300

    add devise to project

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,8 @@
 class Course < ApplicationRecord
+  belongs_to :user
+  
   validates :title, presence: true
   validates :description, presence: true
-
+  
   has_rich_text :description
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 require 'faker'
 
+user = User.create!(email: 'admin@example.com', password:'admin@example.com', password_confirmation: 'admin@example.com')
+
 30.times do
   Course.create({
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::SouthPark.quote
+    description: Faker::TvShows::SouthPark.quote,
+    user = user
   })
 end
