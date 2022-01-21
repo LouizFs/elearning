51bb59169ce45a155ee09463e6cd4dfab12b878f
Luiz <silva.luiz@smartfit.com>
2022-01-21 01:02:58 -0300

    config sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,9 +5,9 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => Rails.application.credentials.dig(:user_name),
-    :password => Rails.application.credentials.dig(:password),
-    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'erails-learning.herokuapp.com',
     :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
