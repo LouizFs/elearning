44ed0c59234322cf01d330b4094b5968a405dc90
Luiz Felipe Silva <luizfelipesilva23@hotmail.com>
2022-01-25 17:13:37 -0300

    change url for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => "rocky-wave-14241.herokuapp.com" }
 
 
   # Enable/disable caching. By default caching is disabled.
