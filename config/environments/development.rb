ded210cdbc2e43eee8e81c92ba79a5f6e60181e7
Luiz Felipe Silva <luizfelipesilva23@hotmail.com>
2022-01-25 17:04:37 -0300

    configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.hosts.clear
-  
+
   config.action_mailer.delivery_method = :smtp
 
   # Do not eager load code on boot.
@@ -16,6 +16,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
